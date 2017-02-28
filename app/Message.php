10123f697f0abc5da4135698e9112c6669aca250
Marc <mcalafellsmx@gmail.com>
2017-02-28 20:14:40 +0100

    TMTFOC

--- app/Message.php
+++ app/Message.php
@@ -4,7 +4,21 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Message extends Model
-{
+class Message extends Model {
+
+    protected $fillable = [
+        'message'
+    ];
+
+    /**
+     * A message belong to a user
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     //
 }
